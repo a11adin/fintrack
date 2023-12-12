7239f79f11a3e61ba412d3b06098bdcd05594300
Renat <rmamedov1234@gmail.com>
2023-12-12 15:41:30 +0300

    Initial commit

---
+++ src/test/java/ru/renatrenat/fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.renatrenat.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
